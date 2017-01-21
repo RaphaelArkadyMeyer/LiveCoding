4421e76b768c999206222d738a291f1cb04d584e
Raphael Arakdy Meyer <Meyer219@purdue.edu>
2017-01-21 00:26:24 -0500

    Completed python quicksort example. Added more test cases, unformatted.

--- Examples/quicksort.py
+++ Examples/quicksort.py
@@ -1,24 +1,30 @@
-from sys import stdin, stdout
+from sys import stdin
 
-
+@@ begin hide
 def main():
-    length = int(stdin.readline())
+    stdin.readline()  # Ignore first line with number of inputs on it
     array_in = stdin.readline()
-    print(map(type,map(int,array_in.split(' '))))
-    print(array_in.split(' '))
+    presort = map(int, array_in.split(' '))
+    sort = quickSort(presort)
+    print(sort)
+@@ end hide
 
-
-if __name__ == '__main__':
-    main()
-
+@@ begin problem
+@@ description: The Classic QuickSort Problem!
 def quickSort(arr):
     less = []
     pivotList = []
     more = []
     if len(arr) <= 1:
+@@ begin question
+@@ description: Find the base case
+@@ points: 100
         return arr
+@@ end question
     else:
         pivot = arr[0]
+@@ begin question
+@@ points: 500
         for i in arr:
             if i < pivot:
                 less.append(i)
@@ -26,10 +32,13 @@ def quickSort(arr):
                 more.append(i)
             else:
                 pivotList.append(i)
+@@ end question
         less = quickSort(less)
         more = quickSort(more)
         return less + pivotList + more
+@@ end problem
 
-a = [4, 65, 2, -31, 0, 99, 83, 782, 1]
-a = quickSort(a)
-print(a)
+@@ begin hide
+if __name__ == '__main__':
+    main()
+@@ begin hide
